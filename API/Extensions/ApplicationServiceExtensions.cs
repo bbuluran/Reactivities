b2b0641855f930165f3b76be7b17f567326365bd
bbuluran <benjie.buluran@gmail.com>
2022-11-15 17:24:45 +0800

    End of Session 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,32 @@
+using Application.Activities;
+using Application.Core;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(options => {
+                options.AddPolicy("react", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(List.Handler).Assembly);
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
